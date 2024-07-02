adefef045926337c64194a1531fbd451968cb6a2
diego borja <diegoborjarivas@gmail.com>
2024-07-02 19:09:08 +0200

    Working on the 'Highlights' section.

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,22 @@
-const Highlights = () => {
-  return (
-    <div>Highlights</div>
-  )
-}
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
 
-export default Highlights
+const Highlights = () => {
+  useEffect(() => {
+    gsap.to('#title', { opacity: 1, y: 0 });
+  }, []);
+
+  return (
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
